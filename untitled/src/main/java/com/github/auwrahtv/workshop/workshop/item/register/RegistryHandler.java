0a6c7c4984c398395cccd92c2737ec404991a133
AuwrahTV <matthias.clain@epitech.eu>
2023-02-01 11:34:43 +0400

    Final push

---
+++ untitled/src/main/java/com/github/auwrahtv/workshop/workshop/item/register/RegistryHandler.java
@@ -0,0 +1,8 @@
+package com.github.auwrahtv.workshop.workshop.item.register;
+
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@SubscribeEvent
+
+public class RegistryHandler {
+}
